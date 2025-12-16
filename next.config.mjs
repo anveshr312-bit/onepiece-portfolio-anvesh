9eb9bdb6390b9e287770bbb1fe3fee3c964fa841
Anvesh Rathore <anveshr312@gmail.com>
2025-12-16 14:44:44 +0530

    Disable ESLint during build for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    output: 'export',
     images: {
         unoptimized: true,
     },
     eslint: {
         ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
     },
 };
 
